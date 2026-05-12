d0f8863e2f9321051e668258a502184de302165a
HMGS2526 <hecmonnic@alu.edu.gva.es>
2026-05-12 12:50:43 +0200

    commit solucion tablas

--- Backend/backend/src/customers/customers.service.ts
+++ Backend/backend/src/customers/customers.service.ts
@@ -1,45 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Customer } from './entities/customer.entity';
+import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 
 @Injectable()
 export class CustomersService {
-  constructor(
-    @InjectRepository(Customer)
-    private readonly customersRepository: Repository<Customer>,
-  ) {}
+  create(createCustomerDto: CreateCustomerDto) {
+    return 'This action adds a new customer';
+  }
 
   findAll() {
-    return this.customersRepository.find();
+    return `This action returns all customers`;
   }
 
   findOne(id: number) {
-    return this.customersRepository.findOneBy({ id });
+    return `This action returns a #${id} customer`;
   }
 
-  create(createCustomerDto: CreateCustomerDto) {
-    const customer = this.customersRepository.create(createCustomerDto);
-    return this.customersRepository.save(customer);
+  update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    return `This action updates a #${id} customer`;
   }
 
-  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    const customer = await this.customersRepository.findOneBy({ id });
-    if (!customer) {
-      throw new NotFoundException(`No existe el cliente con id ${id}`);
-    }
-    const updated = this.customersRepository.merge(customer, updateCustomerDto);
-    return this.customersRepository.save(updated);
-  }
-
-  async remove(id: number) {
-    const customer = await this.customersRepository.findOneBy({ id });
-    if (!customer) {
-      throw new NotFoundException(`No existe el cliente con id ${id}`);
-    }
-    await this.customersRepository.remove(customer);
-    return { message: `Cliente ${id} eliminado correctamente` };
+  remove(id: number) {
+    return `This action removes a #${id} customer`;
   }
 }
