cacd488d528b8a111fd1f87ef97579ac64f44bff
Sergio <sergio16x09@gmail.com>
2026-05-26 10:38:14 +0200

    Admin ahora puede editar clientes y negocios

--- Backend/backend/src/business/business.service.ts
+++ Backend/backend/src/business/business.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Business } from './entities/business.entity';
 import { CreateBusinessDto } from './dto/create-business.dto';
+import { UpdateBusinessDto } from './dto/update-business.dto';
 import { UsersService } from '../users/users.service';
 import { UserRole } from '../users/entities/user.entity';
 
@@ -33,6 +34,11 @@ export class BusinessService {
   }
 
   findOne(id: number) {
+    return this.businessRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateBusinessDto: UpdateBusinessDto) {
+    await this.businessRepository.update(id, updateBusinessDto);
     return this.businessRepository.findOneBy({ id });
   }
 
